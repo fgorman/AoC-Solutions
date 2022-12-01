752990011dca9929d61ece6aaa98666973fd43dc
Foster Gorman <mh337.fg@gmail.com>
2022-12-01 14:10:48 -0500

    Change from HashMap to Vec

--- aoc-lib/src/solutions/aoc_1.rs
+++ aoc-lib/src/solutions/aoc_1.rs
@@ -1,74 +1,47 @@
-use std::collections::HashMap;
-
 pub fn solution(input: String) {
     // Part 1
-    let calories_per_elf = get_calories_per_elf(input);
-    let highest_calories_held = get_highest_calories_held(&calories_per_elf);
+    let mut calories_per_elf = get_calories_per_elf(input);
+    let highest_calories_held = get_highest_calories_held(&mut calories_per_elf);
     println!("Highest calories held by one elf: {}", highest_calories_held);
 
     // Part 2
-    let top_3_elves = get_top_three_elves(&calories_per_elf);
-    let sum_top_3_elves = sum_top_three_elves(top_3_elves);
+    let sum_top_3_elves = sum_top_three_elves(&mut calories_per_elf);
     println!("Sum of top 3 elves: {}", sum_top_3_elves);
 }
 
-fn get_calories_per_elf(calories_list: String) -> HashMap<u32, u32> {
-    let mut calories_per_elf: HashMap<u32, u32> = HashMap::new();
+fn get_calories_per_elf(calories_list: String) -> Vec<u32> {
+    let mut calories_per_elf: Vec<u32> = Vec::new();
 
-    let mut elf_number = 0_u32;
+    let mut calories = 0_u32;
 
     for line in calories_list.lines() {
         if line.is_empty() {
-            elf_number += 1;
+            calories_per_elf.push(calories);
+            calories = 0;
             continue;
         }
 
         let line_to_u32_res = line.parse::<u32>();
 
-        let calories = match line_to_u32_res {
+        calories += match line_to_u32_res {
             Ok(res) => res,
             Err(err) => panic!("Unable to convert line to u32: {}", err),
         };
-
-        *calories_per_elf.entry(elf_number).or_insert(0) += calories;
     }
 
     calories_per_elf
 }
 
-fn get_highest_calories_held(calories_per_elf: &HashMap<u32, u32>) -> u32 {
-    let mut highest_calories_held = u32::MIN;
+fn get_highest_calories_held(calories_per_elf: &mut Vec<u32>) -> u32 {
+    calories_per_elf.sort();
 
-    for (_, calories) in calories_per_elf {
-        if *calories > highest_calories_held {
-            highest_calories_held = *calories;
-        }
-    }
-
-    highest_calories_held
+    calories_per_elf[calories_per_elf.len()-1]
 }
 
-fn get_top_three_elves(calories_per_elf: &HashMap<u32, u32>) -> (u32, u32, u32) {
-    let mut elf_1 = u32::MIN;
-    let mut elf_2 = u32::MIN;
-    let mut elf_3 = u32::MIN;
+fn sum_top_three_elves(calories_per_elf: &mut Vec<u32>) -> u32 {
+    calories_per_elf.sort();
 
-    for (_, calories) in calories_per_elf {
-        if *calories > elf_1 {
-            elf_3 = elf_2;
-            elf_2 = elf_1;
-            elf_1 = *calories;
-        } else if *calories > elf_2 {
-            elf_3 = elf_2;
-            elf_2 = *calories;
-        } else if *calories > elf_3 {
-            elf_3 = *calories;
-        }
-    }
+    let n = calories_per_elf.len();
 
-    (elf_1, elf_2, elf_3)
-}
-
-fn sum_top_three_elves(top_3: (u32, u32, u32)) -> u32 {
-    top_3.0 + top_3.1 + top_3.2
+    calories_per_elf[n-3] + calories_per_elf[n-2] + calories_per_elf[n-1]
 }
