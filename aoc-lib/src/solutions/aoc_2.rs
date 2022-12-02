bb777711d0454bf1256087bdfee6eeaf4c1d2899
Foster Gorman <mh337.fg@gmail.com>
2022-12-02 18:36:52 -0500

    Change day 2 to use strat hashmaps

--- aoc-lib/src/solutions/aoc_2.rs
+++ aoc-lib/src/solutions/aoc_2.rs
@@ -1,89 +1,45 @@
+use std::collections::HashMap;
+
 pub fn solution(input: String) {
     // Part 1
-    let score_p1 = get_score_part_1(&input);
+    let mut part_1_strat = HashMap::new();
+    part_1_strat.insert("A X", 4);
+    part_1_strat.insert("A Y", 8);
+    part_1_strat.insert("A Z", 3);
+    part_1_strat.insert("B X", 1);
+    part_1_strat.insert("B Y", 5);
+    part_1_strat.insert("B Z", 9);
+    part_1_strat.insert("C X", 7);
+    part_1_strat.insert("C Y", 2);
+    part_1_strat.insert("C Z", 6);
+
+    let score_p1 = get_score(&input, &part_1_strat);
     println!("Part 1 score = {}", score_p1);
 
-    let score_p2 = get_score_part_2(&input);
+    // Part 2
+    let mut part_2_strat = HashMap::new();
+    part_2_strat.insert("A X", 3);
+    part_2_strat.insert("A Y", 4);
+    part_2_strat.insert("A Z", 8);
+    part_2_strat.insert("B X", 1);
+    part_2_strat.insert("B Y", 5);
+    part_2_strat.insert("B Z", 9);
+    part_2_strat.insert("C X", 2);
+    part_2_strat.insert("C Y", 6);
+    part_2_strat.insert("C Z", 7);
+
+    let score_p2 = get_score(&input, &part_2_strat);
     println!("Part 2 score = {}", score_p2);
 }
 
-fn get_score_part_1(input: &String) -> u32 {
+fn get_score(input: &String, strategy: &HashMap<&str, u32>) -> u32 {
     // A = Rock ; B = Paper ; C = Scissors
     // X = Rock ; Y = Paper ; Z = Scissors
 
     let mut score = 0_u32;
 
     for line in input.lines() {
-        let split_line = line.split_whitespace().into_iter().collect::<Vec<&str>>();
-        let their_pick = split_line[0];
-        let our_pick = split_line[1];
-
-        if their_pick == "A" {
-            if our_pick == "X" {
-                score += 1 + 3;
-            } else if our_pick == "Y" {
-                score += 2 + 6;
-            } else if our_pick == "Z" {
-                score += 3;
-            }
-        } else if their_pick == "B" {
-            if our_pick == "X" {
-                score += 1;
-            } else if our_pick == "Y" {
-                score += 2 + 3;
-            } else if our_pick == "Z" {
-                score += 3 + 6;
-            }
-        } else if their_pick == "C" {
-            if our_pick == "X" {
-                score += 1 + 6;
-            } else if our_pick == "Y" {
-                score += 2;
-            } else if our_pick == "Z" {
-                score += 3 + 3;
-            }
-        }
-    }
-
-    score
-}
-
-fn get_score_part_2(input: &String) -> u32 {
-    // A = Rock ; B = Paper ; C = Scissors
-    // X = Lose ; Y = Draw ; Z = Win
-
-    let mut score = 0_u32;
-
-    for line in input.lines() {
-        let split_line = line.split_whitespace().into_iter().collect::<Vec<&str>>();
-        let their_pick = split_line[0];
-        let our_pick = split_line[1];
-
-        if their_pick == "A" {
-            if our_pick == "X" {
-                score += 3;
-            } else if our_pick == "Y" {
-                score += 1 + 3;
-            } else if our_pick == "Z" {
-                score += 2 + 6;
-            }
-        } else if their_pick == "B" {
-            if our_pick == "X" {
-                score += 1;
-            } else if our_pick == "Y" {
-                score += 2 + 3;
-            } else if our_pick == "Z" {
-                score += 3 + 6;
-            }
-        } else if their_pick == "C" {
-            if our_pick == "X" {
-                score += 2;
-            } else if our_pick == "Y" {
-                score += 3 + 3;
-            } else if our_pick == "Z" {
-                score += 1 + 6;
-            }
-        }
+        score += strategy[line];
     }
 
     score
